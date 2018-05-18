7d4769eefcf772f8212b8a026ca8c25ba2300a04
kurokoji <shu.kakihana@gmail.com>
2018-05-18 23:10:31 +0900

    [add] 型エイリアス用のtype.hを追加した

---
+++ mikuru/type.h
@@ -0,0 +1,23 @@
+#ifndef MIKURU_TYPES_H_
+#define MIKURU_TYPES_H_
+
+#include <cstdint>
+namespace mikuru {
+/*
+ * Integer types
+ */
+
+using int8 = std::int_fast8_t;
+using int16 = std::int_fast16_t;
+using int32 = std::int_fast32_t;
+using int64 = std::int_fast64_t;
+
+/*
+ * Character types
+ */
+
+using char8 = char;
+using char16 = char16_t;
+using char32 = char32_t;
+} // namespace mikuru
+#endif
