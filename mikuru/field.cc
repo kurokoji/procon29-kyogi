f788d7b96f908fa2962ace47a277df5f71ff81a3
kurokoji <shu.kakihana@gmail.com>
2018-05-22 09:31:37 +0900

    [add] field.hとfield.ccを追加した

---
+++ mikuru/field.cc
@@ -0,0 +1,13 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+namespace mikuru {
+} // namespace mikuru
---
+++ mikuru/field.h
@@ -0,0 +1,18 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#ifndef MIKURU_FIELD_H_
+#define MIKURU_FIELD_H_
+
+namespace mikuru {
+} // namespace mikuru
+
+#endif
