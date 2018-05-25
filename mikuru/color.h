164ecf89f4c47d8f9cc0866f556e185babb5e300
kurokoji <shu.kakihana@gmail.com>
2018-05-26 00:49:59 +0900

    [add] 色(チーム)のenum classを定義したcolor.hを追加した

---
+++ mikuru/color.h
@@ -0,0 +1,18 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#ifndef MIKURU_COLOR_H
+#define MIKURU_COLOR_H
+
+namespace mikuru {
+} // namespace mikuru
+
+#endif
