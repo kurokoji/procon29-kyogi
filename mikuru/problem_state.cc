c273586c69da733bc6b934acef2a7ea1346839c1
kurokoji <shu.kakihana@gmail.com>
2018-05-26 01:01:46 +0900

    [add] 問題の状態を扱うProblemStateクラスを定義したproblem_state.hとproblem_state.ccを追加した

---
+++ mikuru/problem_state.cc
@@ -0,0 +1,16 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#include "problem_state.h"
+
+namespace mikuru {
+
+} // namespace mikuru
---
+++ mikuru/problem_state.h
@@ -0,0 +1,23 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#ifndef MIKURU_PROBLEM_STATE_H_
+#define MIKURU_PROBLEM_STATE_H_
+
+#include "color.h"
+#include "color_field.h"
+#include "field.h"
+#include "types.h"
+
+namespace mikuru {
+} // namespace mikuru
+
+#endif
