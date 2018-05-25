b7c412bf8d0b93e62bcc8a40cafa916ff685e7e3
kurokoji <shu.kakihana@gmail.com>
2018-05-26 00:57:03 +0900

    [add] どのマスが塗られているかを扱うColorFieldクラスを定義するcolor_field.hとcolor_field.ccを追加した

---
+++ mikuru/color_field.cc
@@ -0,0 +1,15 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#include "color_field.h"
+
+namespace mikuru {
+} // namespace mikuru
---
+++ mikuru/color_field.h
@@ -0,0 +1,23 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#ifndef MIKURU_COLOR_FIELD_H_
+#define MIKURU_COLOR_FIELD_H_
+
+#include "color.h"
+#include "field.h"
+#include <array>
+
+namespace mikuru {
+class ColorField : public Field {
+} // namespace mikuru
+
+#endif
