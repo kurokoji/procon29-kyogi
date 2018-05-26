a34cef5fb95c1b7b7e2739a82e75d05038c281fb
kurokoji <shu.kakihana@gmail.com>
2018-05-27 02:16:37 +0900

    [update] ProblemStateクラスのコンストラクタと<<,>>演算子のオーバーロードを定義した

--- mikuru/problem_state.cc
+++ mikuru/problem_state.cc
@@ -10,7 +10,43 @@
 //====================================================
 
 #include "problem_state.h"
+#include <cmath>
+#include <cstdlib>
 
 namespace mikuru {
+/*
+ * public
+ */
 
+ProblemState::ProblemState() {}
+std::ostream& operator<<(std::ostream& os, ProblemState const& problem) {
+  os << problem.field_ << problem.color_field_;
+
+  for (const auto& e : problem.blue_) {
+    os << e.first << " " << e.second << '\n';
+  }
+
+  for (const auto& e : problem.red_) {
+    os << e.first << " " << e.second << '\n';
+  }
+
+  return os;
+}
+std::istream& operator>>(std::istream& is, ProblemState& problem) {
+  is >> problem.field_ >> problem.color_field_;
+
+  for (size_t i = 0; i < 2; ++i) {
+    auto& [blue_h, blue_w] = problem.blue_.at(i);
+    auto& [red_h, red_w] = problem.red_.at(i);
+    int32 h, w;
+    is >> h >> w;
+    blue_h = static_cast<size_t>(h);
+    blue_w = static_cast<size_t>(w);
+    red_h = static_cast<size_t>(
+        std::abs(h - static_cast<int32>(problem.field_.getHeight()) - 1));
+    red_w = static_cast<size_t>(
+        std::abs(w - static_cast<int32>(problem.field_.getWidth()) - 1));
+  }
+  return is;
+}
 } // namespace mikuru
--- mikuru/problem_state.h
+++ mikuru/problem_state.h
@@ -16,8 +16,24 @@
 #include "color_field.h"
 #include "field.h"
 #include "types.h"
+#include <array>
+#include <istream>
+#include <ostream>
+#include <utility>
 
 namespace mikuru {
+class ProblemState {
+private:
+  Field field_;
+  ColorField color_field_;
+  std::array<std::pair<size_t, size_t>, 2> blue_, red_;
+
+public:
+  ProblemState();
+
+  friend std::ostream& operator<<(std::ostream&, ProblemState const&);
+  friend std::istream& operator>>(std::istream&, ProblemState&);
+};
 } // namespace mikuru
 
 #endif
