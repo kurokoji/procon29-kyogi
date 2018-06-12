39844bd71bf065a8eb73ace0e6cbebba19c54e4a
kurokoji <shu.kakihana@gmail.com>
2018-06-12 09:37:28 +0900

    [add] qrコードの撮影とデコードの実装を分けるためにqr.cppとqr.hppを作った

---
+++ mikuru/qr.cpp
@@ -0,0 +1,24 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#include "qr.hpp"
+#include "types.hpp"
+#include <string>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <zbar.h>
+#include <opencv2/opencv.hpp>
+
+namespace mikuru {
+std::string get_qr_string(size_t cam_num) {
+}
+} // namespace mikuru
---
+++ mikuru/qr.hpp
@@ -0,0 +1,21 @@
+//====================================================
+//
+//  mikuru
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the MIT License.
+//  https://opensource.org/licenses/mit-license.php
+//
+//====================================================
+
+#ifndef MIKURU_QR_H_
+#define MIKURU_QR_H_
+
+#include <string>
+
+namespace mikuru {
+std::string get_qr_string(size_t cam_num = 0);
+} // namespace mikuru
+
+#endif
