7338e53869290f220b1c84b99e02055a79574579
kurokoji <shu.kakihana@gmail.com>
2018-06-14 23:00:21 +0900

    [add] types.hppに整数型，文字型などのaliasを定義した

--- nagato/nagato/types.hpp
+++ nagato/nagato/types.hpp
@@ -12,7 +12,31 @@
 #ifndef NAGATO_TYPES_HPP_
 #define NAGATO_TYPES_HPP_
 
+#include <cstdint>
+
 namespace nagato {
+/*
+ * Integer types
+ */
+
+using int8 = std::int_fast8_t;
+using int16 = std::int_fast16_t;
+using int32 = std::int_fast32_t;
+using int64 = std::int_fast64_t;
+
+/*
+ * Character types
+ */
+
+using char8 = char;
+using char16 = char16_t;
+using char32 = char32_t;
+
+/*
+ * Score type
+ */
+
+using score_type = int32;
 } // namespace nagato
 
 #endif
