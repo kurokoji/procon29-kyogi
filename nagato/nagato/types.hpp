c912c8d26ec219fc5ce504fdc446df31f7f351d6
kurokoji <shu.kakihana@gmail.com>
2018-06-14 22:59:38 +0900

    [add] 型のaliasを定義したtypes.hppを作った

---
+++ nagato/nagato/types.hpp
@@ -0,0 +1,18 @@
+//====================================================
+//
+//  nagato
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the GNU General Public License version 3.
+//  https://opensource.org/licenses/GPL-3.0
+//
+//====================================================
+
+#ifndef NAGATO_TYPES_HPP_
+#define NAGATO_TYPES_HPP_
+
+namespace nagato {
+} // namespace nagato
+
+#endif
