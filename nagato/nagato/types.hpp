1266218acb37ee92198a23f16a96d93d3f645237
kurokoji <shu.kakihana@gmail.com>
2018-06-21 21:06:39 +0900

    [change] 符号なし整数型aliasでui32のようにしていたが，u32のように変更

--- nagato/nagato/types.hpp
+++ nagato/nagato/types.hpp
@@ -24,10 +24,10 @@ using i8 = std::int_fast8_t;
 using i16 = std::int_fast16_t;
 using i32 = std::int_fast32_t;
 using i64 = std::int_fast64_t;
-using ui8 = std::uint_fast8_t;
-using ui16 = std::uint_fast16_t;
-using ui32 = std::uint_fast32_t;
-using ui64 = std::uint_fast64_t;
+using u8 = std::uint_fast8_t;
+using u16 = std::uint_fast16_t;
+using u32 = std::uint_fast32_t;
+using u64 = std::uint_fast64_t;
 
 /*
  * Float types
