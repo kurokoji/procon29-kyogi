5d3ef72d29029e56fec03847e9a69b8736cf8337
kurokoji <shu.kakihana@gmail.com>
2018-06-21 23:42:00 +0900

    [add] fieldの基本的なメンバとコンストラクタを追加

--- nagato/nagato/field.hpp
+++ nagato/nagato/field.hpp
@@ -13,10 +13,21 @@
 #define NAGATO_FIELD_HPP_
 
 #include "types.hpp"
+#include <array>
 
 namespace nagato {
 template <typename T>
 class field {
+protected:
+  static constexpr u32 MAX_FIELD_SIZE = 12;
+  u32 height_, width_;
+
+private:
+  std::array<std::array<T, MAX_FIELD_SIZE>, MAX_FIELD_SIZE> field_;
+
+public:
+  field() noexcept : height_(0), width_(0) {}
+  field(u32 h, u32 w) noexcept : height_(h), width_(w) {}
 };
 }  // namespace nagato
 
