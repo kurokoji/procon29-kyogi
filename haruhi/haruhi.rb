374968e760ff8de220658bc69f0a154a4323269d
kurokoji <shu.kakihana@gmail.com>
2018-07-16 19:35:08 +0900

    [add] 基本的なserverの機能を実装した

--- haruhi/haruhi.rb
+++ haruhi/haruhi.rb
@@ -1,6 +1,20 @@
 require 'socket'
 
+Port = 20000
+
 def main()
+    problem_str = ''
+    server = TCPServer.open(Port)
+
+    loop do
+        client = server.accept
+        puts 'start'
+        while res = client.gets
+            puts res
+            res.chomp!
+        end
+        client.close
+    end
 end
 
 if __FILE__ == $0
