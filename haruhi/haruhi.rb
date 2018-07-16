a169db6efb136b2741a23f058bdb30f3984f1555
kurokoji <shu.kakihana@gmail.com>
2018-07-16 19:36:07 +0900

    [add] 問題情報を要求・送信するプロコトルを実装した

--- haruhi/haruhi.rb
+++ haruhi/haruhi.rb
@@ -12,6 +12,20 @@ def main()
         while res = client.gets
             puts res
             res.chomp!
+            if res == 'POST problem'
+                problem_str = ''
+                while buf = client.gets
+                    problem_str << buf
+                end
+            end
+
+            if res == 'GET problem'
+                if problem_str != ''
+                    client.puts(problem_str)
+                else
+                    client.puts('NG')
+                end
+            end
         end
         client.close
     end
