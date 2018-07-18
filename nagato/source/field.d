e26e5b29cc9b64e5ea759b142d62d217cbbee2a3
kurokoji <shu.kakihana@gmail.com>
2018-07-18 15:21:08 +0900

    [add] field.dを追加

---
+++ nagato/source/field.d
@@ -0,0 +1,18 @@
+//====================================================
+//
+//  nagato
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the GNU General Public License version 3.
+//  https://opensource.org/licenses/GPL-3.0
+//
+//====================================================
+
+module nagato.field;
+
+struct Field(T)
+{
+}
+
+
