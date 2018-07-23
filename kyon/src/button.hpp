ad66794e025c4a0e24d7a54b10e62162fbe005ef
higashi000 <higashisystem@gmail.com>
2018-07-23 18:21:35 +0900

    [add] Button class

---
+++ kyon/src/button.hpp
@@ -0,0 +1,13 @@
+#ifndef PROCON29_KYON_BUTTON_HPP_
+#define PROCON29_KYON_BUTTON_HPP_
+
+#include <Siv3D.hpp>
+
+struct Button : public Rect {
+  int clicknum = 0;
+  Rect rect = Rect(0, 0, 60, 60);
+  bool canClick();
+  int click();
+};
+
+#endif
