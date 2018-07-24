e387ae0e0f8a0e26a25617060f592788f8b3db33
nemusou <ryubi.102395@gmail.com>
2018-07-24 22:13:55 +0900

    [clean] インクルードガードが役目を果たしていなかったので直した

--- kyon/src/Game.hpp
+++ kyon/src/Game.hpp
@@ -1,6 +1,5 @@
-#ifndef PROCON29_KYON_Game_HPP_
-#define PROCON29_KYON_Game_HPP_
-#endif
+#ifndef PROCON29_KYON_GAME_HPP_
+#define PROCON29_KYON_GAME_HPP_
 
 #include <Siv3D.hpp>
 #include <HamFramework.hpp>
@@ -29,3 +28,5 @@ public:
         Rect(120, 120, 40, 40).draw();
     }
 };
+
+#endif
