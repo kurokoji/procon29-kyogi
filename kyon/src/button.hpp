c57728e796f23e71ee33212d6a0db9340ebab87d
higashi000 <higashisystem@gmail.com>
2018-07-28 18:55:03 +0900

    [add] クリック機能を`b.click()`で使えるようにした

--- kyon/src/button.hpp
+++ kyon/src/button.hpp
@@ -7,13 +7,15 @@ struct Button {
   Rect rect;
   Font font;
   Vec2 pos;
+  uint32 clickNum = 0;
 
   Button();
   Button(const Rect& rect, const Font& font);
   Button(uint32 h, uint32 w);
 
   Button& setPos(uint32 x, uint32 y);
-  Button& draw(const String& str, const ColorF& color = Palette::White);
+  Button& draw(const String& str);
+  Button& click();
 };
 
 #endif
