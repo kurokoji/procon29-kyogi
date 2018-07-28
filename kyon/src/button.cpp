178bc8dc2f235877025a5503dd8e8adc59e6e55a
higashi000 <higashisystem@gmail.com>
2018-07-28 18:55:29 +0900

    [add] クリック機能を`b.click()`で使えるようにした

--- kyon/src/button.cpp
+++ kyon/src/button.cpp
@@ -1,20 +1,36 @@
 #include "button.hpp"
 #include <Siv3D.hpp>
 
-Button::Button() : rect(0, 0, 60, 60), font(40), pos(60, 60) {}
+Button::Button() : rect(0, 0, 60, 60), font(40), pos(60, 60), clickNum(0) {}
 Button::Button(const Rect& rect, const Font& font) : rect(rect), font(font) {}
 Button::Button(uint32 h, uint32 w) : rect(0, 0, h, w), font(40), pos(h, w) {}
 
 Button& Button::setPos(uint32 x, uint32 y) {
-    pos = Vec2(x, y);
-    rect.setPos(x, y);
-    //font.draw(x, y, Palette::White);
-    return *this;
+  pos = Vec2(x, y);
+  rect.setPos(x, y);
+  return *this;
 }
 
-Button& Button::draw(const String& str, const ColorF& color) {
-    rect.draw(color);
-    font(str).draw(pos, Palette::White);
-    return *this;
+Button& Button::draw(const String& str) {
+  if (clickNum == 0) {
+    rect.draw(Palette::White);
+  } else if (clickNum == 1) {
+    rect.draw(Palette::Red);
+  } else if (clickNum == 2){
+    rect.draw(Palette::Blue);
+  }
+  font(str).draw(pos, Palette::Black);
+  return *this;
 }
 
+Button& Button::click() {
+  if (rect.leftClicked()) {
+    clickNum++;
+  }
+
+  if (clickNum == 3) {
+    clickNum = 0;
+  }
+
+  return *this;
+}
