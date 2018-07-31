80e176cd11598be209b3bc79681f6982d93cc5d7
kurokoji <shu.kakihana@gmail.com>
2018-07-31 14:22:26 +0900

    [add] algorithm配下のmoduleをすべてimportするためにpackage.dを置いた

---
+++ nagato/source/algorithm/package.d
@@ -0,0 +1,13 @@
+//====================================================
+//
+//  nagato
+//
+//  Copyright (c) 2018 Shu Kakihana
+//
+//  This software is released under the GNU General Public License version 3.
+//  https://opensource.org/licenses/GPL-3.0
+//
+//====================================================
+
+module nagato.algorithm;
+
