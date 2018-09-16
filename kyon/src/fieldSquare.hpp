05ade0c72f47cb6ce364fd4e8911ed73fff9384f
higashi000 <higashisystem@gmail.com>
2018-09-16 13:44:17 +0900

    [add] `setPos` `update` を追加

--- kyon/src/fieldSquare.hpp
+++ kyon/src/fieldSquare.hpp
@@ -2,6 +2,14 @@
 
 class FieldSquare : public Button {
 public:
+  Rect choiceWhiteSquare;
+  Rect choiceRedSquare;
+  Rect choiceBlueSquare;
+  bool isSquareDisp;
+
+  FieldSquare();
+  FieldSquare& setPos(uint32 x, uint32 y);
   FieldSquare& draw(const String& str, bool& hasAgent);
   void normalSquare();
+  void update(bool& hasAgent);
 };
