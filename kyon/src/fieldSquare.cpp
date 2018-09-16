d7e32c5784a52facea3396963348ddbc90f09b67
higashi000 <higashisystem@gmail.com>
2018-09-16 13:45:31 +0900

    [add] 色の選択方法を変更

--- kyon/src/fieldSquare.cpp
+++ kyon/src/fieldSquare.cpp
@@ -1,10 +1,21 @@
 #include "fieldSquare.hpp"
 
+FieldSquare::FieldSquare() : choiceWhiteSquare(0, 0, 30, 30), choiceRedSquare(0, 0, 30, 30), choiceBlueSquare(0, 0, 30, 30), isSquareDisp(false) {}
+
+FieldSquare& FieldSquare::setPos(uint32 x, uint32 y) {
+  pos = Vec2(x, y);
+  rect.setPos(x, y);
+  choiceWhiteSquare.setPos(x + 60, y);
+  choiceRedSquare.setPos(x + 60, y + 60);
+  choiceBlueSquare.setPos(x + 60, y + 120);
+
+  return *this;
+}
+
 FieldSquare& FieldSquare::draw(const String& str, bool& hasAgent) {
   if (hasAgent) {
     rect.draw(Palette::Green);
     if (rect.leftClicked()) {
-      hasAgent = false;
       normalSquare();
     }
   } else {
@@ -13,6 +24,31 @@ FieldSquare& FieldSquare::draw(const String& str, bool& hasAgent) {
   font(str).draw(pos, Palette::Black);
 
   return *this;
+}
+
+void FieldSquare::update(bool& hasAgent) {
+  if (hasAgent) {
+    if (rect.leftClicked() || isSquareDisp == true) {
+      isSquareDisp = true;
+      choiceWhiteSquare.draw(Palette::White).drawFrame(0, 5, Palette::Yellow);
+      choiceRedSquare.draw(Palette::Red).drawFrame(0, 5, Palette::Yellow);
+      choiceBlueSquare.draw(Palette::Blue).drawFrame(0, 5, Palette::Yellow);
+
+      if (choiceWhiteSquare.leftClicked()) {
+        clickNum = 0;
+        hasAgent = false;
+        isSquareDisp = false;
+      } else if (choiceRedSquare.leftClicked()) {
+        clickNum = 1;
+        hasAgent = false;
+        isSquareDisp = false;
+      } else if (choiceBlueSquare.leftClicked()) {
+        clickNum = 2;
+        hasAgent = false;
+        isSquareDisp = false;
+      }
+    }
+  }
 }
 
 void FieldSquare::normalSquare() {
