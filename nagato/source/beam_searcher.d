e76681866e6402119710b31587841c08f6df74e0
kurokoji <shu.kakihana@gmail.com>
2018-09-19 20:41:14 +0900

    [add] beam_searcherの基本的なところつくった

--- nagato/source/beam_searcher.d
+++ nagato/source/beam_searcher.d
@@ -11,9 +11,62 @@
 
 module nagato.beam_searcher;
 
+import nagato.state;
+
+struct ContextState
+{
+    State st;
+    ContextState* node;
+    this()
+    {
+    }
+
+    this(State s)
+    {
+        st = s;
+    }
+
+    this(State s, ContextState* ptr)
+    {
+        st = s;
+        node = ptr;
+    }
+}
+
+interface SolverStrategy
+{
+    static ContextState*[] nextState(const ref State);
+}
+
+class AllStrategy : SolverStrategy
 {
     static ContextState*[] nextState(const ref State st)
     {
+        import nagato.color : Color;
+
+        ContextState*[] ret;
+
+        foreach (i; 0 .. 9)
+            foreach (j; 0 .. 9)
+                foreach (k; 0 .. 9)
+                    foreach (l; 0 .. 9)
+                    {
+                        auto next = st;
+                        with (next)
+                        {
+                            own[0].trans(i);
+                            own[1].trans(j);
+                            opponent[0].trans(k);
+                            opponent[1].trans(l);
+
+                            if (isValidState())
+                            {
+                                ret ~= new ContextState(next, &st);
+                            }
+                        }
+                    }
+
+        return ret;
     }
 }
 
@@ -24,6 +77,11 @@ struct BeamSearcher(Strategy)
     State _state;
 
     this(ref in State st)
+    {
+        _state = cast(State) st;
+    }
+
+    State solve(State st, uint beamWidth = 100)
     {
     }
 }
