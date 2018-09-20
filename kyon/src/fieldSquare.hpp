98278e0474077d39121a089bfbfadd2b3600c5ba
higashi000 <higashisystem@gmail.com>
2018-09-20 10:25:41 +0900

    [add] キャンセル機能用の変数と関数を追加

--- kyon/src/fieldSquare.hpp
+++ kyon/src/fieldSquare.hpp
@@ -6,10 +6,12 @@ public:
   Rect choiceRedSquare;
   Rect choiceBlueSquare;
   bool isSquareDisp;
+  bool canCancel;
 
   FieldSquare();
   FieldSquare& setPos(uint32 x, uint32 y);
   FieldSquare& draw(const String& str, bool& hasAgent);
   void normalSquare();
-  void update(bool& hasAgent);
+  void choiceColor(bool &canMove, bool& isSquareDisp);
+  void update(bool& canMove);
 };
