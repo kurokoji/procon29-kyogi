b5142510d9d5523a7df57920a60ceea7d568ba9e
nemusou <ryubi.102395@gmail.com>
2018-09-29 22:44:21 +0900

    [clean] format

--- kyon/src/Game.hpp
+++ kyon/src/Game.hpp
@@ -1,44 +1,38 @@
 #ifndef PROCON29_KYON_GAME_HPP_
 #define PROCON29_KYON_GAME_HPP_
 
-#include <iostream>
-#include <tuple>
-#include <string>
-#include <sstream>
-#include <Siv3D.hpp>
 #include <HamFramework.hpp>
+#include <Siv3D.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <tuple>
 #include "button.hpp"
 
 namespace kyon {
 
 // 全てのシーンからアクセスできるデータ
 // getData().memberNameで利用できる
-    struct GameData {
-        // int32 memberName = 0;
-    };
+struct GameData {
+  // int32 memberName = 0;
+};
 
-    using App = SceneManager<String, GameData>;
+using App = SceneManager<String, GameData>;
 
-    class Game : public App::Scene {
-    public:
-        // // destructor
-        // virtual ~Game() = default;
+class Game : public App::Scene {
+public:
+  // // destructor
+  // virtual ~Game() = default;
 
-        // // constructor
-        Game(const InitData &init) : IScene(init) {}
+  // // constructor
+  Game(const InitData &init) : IScene(init) {}
 
-        void update() override;
+  void update() override;
+  void draw() const override;
+  String getFieldData();
+  std::tuple<int32, int32, std::vector<std::vector<int32>>> parseFieldData(const String &fieldData);
+};
 
-//    void draw() const override {
-//        Rect(120, 120, 40, 40).draw();
-//    }
-        void draw() const override;
-
-        String getFieldData();
-
-        std::tuple<int32, int32, std::vector<std::vector<int32>>> parseFieldData(const String &fieldData);
-    };
-
-}
+}  // namespace kyon
 
 #endif
--- kyon/src/Main.cpp
+++ kyon/src/Main.cpp
@@ -1,20 +1,20 @@
 ﻿#include "Game.hpp"
-#include <Siv3D.hpp> // OpenSiv3D v0.2.6
 #include <HamFramework.hpp>
+#include <Siv3D.hpp>  // OpenSiv3D v0.2.6
 
 void Main() {
-    Window::Resize(1280, 780);
+  Window::Resize(1280, 780);
 
-    // 画面遷移のマネージャー
-    kyon::App manager;
-    manager.setFadeColor(ColorF(1.0, 1.0, 1.0));
-    manager.add<kyon::Game>(U"Game");
+  // 画面遷移のマネージャー
+  kyon::App manager;
+  manager.setFadeColor(ColorF(1.0, 1.0, 1.0));
+  manager.add<kyon::Game>(U"Game");
 
-    const Font font(50);
+  const Font font(50);
 
-    while (System::Update()) {
-        if (!manager.update()) {
-            break;
-        }
+  while (System::Update()) {
+    if (!manager.update()) {
+      break;
     }
+  }
 }
