7810a3cf615fbf2ec212cb0aeef86d687556948c
madowipe@gmail.com <madowipe@gmail.com>
2018-10-10 23:23:22 +0900

    [add]途中経過、メモを残した

---
+++ kyon/src/Field.cpp
@@ -0,0 +1,32 @@
+#include "Field.hpp"
+#include <Siv3D.hpp>
+
+Field::Field(int32 h, int32 w) : squSize(60), H(h), W(w), image(h, w, Palette::White), texture(image){}
+
+
+void InitField(){
+  //Agentの位置をfieldSquareに渡す
+  //各マスに割り振られた点数をfieldSquareに渡す
+}
+
+//fieldの更新
+void updateField(){
+  //drawされたfieldSquareに変化がある場合(clickされたとき)に呼び出される
+  //マスの色の変化をupdateする
+}
+
+//H * W　マスの描画
+void Field::drawField(const String& str, bool& hasAgent) {
+  texture.scaled(squSize).draw(10, 10);
+
+  for (int i : step(H)) {
+    for (int j : step(W)) {
+      square[i][j].setPos(10 + j * squSize, 10 + i * squSize);
+      square[i][j].draw(str, hasAgent);
+      square[i][j].rect.drawFrame(1.0, 1.0, Palette::Gray);
+
+      texture.fill(image);
+    }
+  }
+  updateField();
+}
---
+++ kyon/src/Field.hpp
@@ -0,0 +1,25 @@
+#ifndef PROCON29_KYON_FIELD_HPP_
+#define PROCON29_KYON_FIELD_HPP_
+
+#include "fieldSquare.hpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <Siv3D.hpp>
+
+class Field {
+private:
+    int32 H, W;
+    int32 squSize;
+    Image image;
+    DynamicTexture texture;
+    std::vector<std::vector<FieldSquare>> square;
+public:
+    Field(int32 h, int32 w);
+
+    void InitField();
+    void updateField();
+    void drawField(const String& str, bool& hasAgent);
+};
+
+#endif
