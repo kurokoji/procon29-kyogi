f19bb663c92c47cfa3257b8deb01e8df4a0ca7a1
kurokoji <shu.kakihana@gmail.com>
2018-10-25 01:05:18 +0900

    [add] moveのプロトコルを追加

--- haruhi/procon29_protocol.rb
+++ haruhi/procon29_protocol.rb
@@ -3,10 +3,12 @@ module Procon29
     module GET
       Problem = 'GET problem'
       Answer = 'GET answer'
+      Move = 'GET move'
     end
     module POST
       Problem = 'POST problem'
       Answer = 'POST answer'
+      Move = 'POST move'
     end
   end
 end
