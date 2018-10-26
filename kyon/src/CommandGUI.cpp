e37683a7d32f892fe03f338413d459cf518167fb
madowipe@gmail.com <madowipe@gmail.com>
2018-10-26 14:18:25 +0900

    [fix]CommandGUIの大きさ調整、指示の表示

--- kyon/src/CommandGUI.cpp
+++ kyon/src/CommandGUI.cpp
@@ -7,67 +7,99 @@ CommandGUI::CommandGUI(int32 recX, int32 recY){
   y = recY;
 }
 
-void CommandGUI::convInt(std::string s){
-  ss << s;
-  ss >> cmd;
-}
-
-void CommandGUI::selectImg(int32 cmd){
-  switch(cmd){
+void CommandGUI::selectImg(int32 b1, int32 b2){
+  switch(b1){
     case 0:
-      rPath = U"../../../image/HandR00.png";
-      lPath = U"../../../image/HandL00.png";
+      nemu = U"../../../image/HandL00.png";
       break;
     case 1:
-      rPath = U"../../../image/HandR01.png";
-      lPath = U"../../../image/HandL00.png";
+      nemu = U"../../../image/HandL01.png";
       break;
     case 2:
-      rPath = U"../../../image/HandR02.png";
-      lPath = U"../../../image/HandL00.png";
+      nemu = U"../../../image/HandL02.png";
       break;
     case 3:
-      rPath = U"../../../image/HandR03.png";
-      lPath = U"../../../image/HandL00.png";
+      nemu = U"../../../image/HandL03.png";
       break;
     case 4:
-      rPath = U"../../../image/HandR04.png";
-      lPath = U"../../../image/HandL00.png";
+      nemu = U"../../../image/HandL04.png";
       break;
     case 5:
-      rPath = U"../../../image/HandR05.png";
-      lPath = U"../../../image/HandL00.png";
+      nemu = U"../../../image/HandL05.png";
       break;
     case 6:
-      rPath = U"../../../image/HandR05.png";
-      lPath = U"../../../image/HandL01.png";
+      nemu = U"../../../image/HandL01.png";
       break;
     case 7:
-      rPath = U"../../../image/HandR05.png";
-      lPath = U"../../../image/HandL02.png";
+      nemu = U"../../../image/HandL02.png";
       break;
     case 8:
-      rPath = U"../../../image/HandR05.png";
-      lPath = U"../../../image/HandL03.png";
+      nemu = U"../../../image/HandL03.png";
+      break;
+    default:
+      break;
+  }
+  switch (b2){
+    case 0:
+      higashi = U"../../../image/HandR00.png";
+      break;
+    case 1:
+      higashi = U"../../../image/HandR01.png";
+      break;
+    case 2:
+      higashi = U"../../../image/HandR02.png";
+      break;
+    case 3:
+      higashi = U"../../../image/HandR03.png";
+      break;
+    case 4:
+      higashi = U"../../../image/HandR04.png";
+      break;
+    case 5:
+      higashi = U"../../../image/HandR05.png";
+      break;
+    case 6:
+      higashi = U"../../../image/HandR01.png";
+      break;
+    case 7:
+      higashi = U"../../../image/HandR02.png";
+      break;
+    case 8:
+      higashi = U"../../../image/HandR03.png";
       break;
     default:
       break;
   }
 }
 
-void CommandGUI::dispCmd(){
-  const Rect rect(x, y, 340, y + 250);
-  const Texture rImg(rPath);
-  const Texture lImg(lPath);
-  const Font rUpp(50);
-  const Font lUpp(50);
+void CommandGUI::dispCmd(int32 b1, int32 b2){
+  const Rect rect(x, y, 380, 270);
+  const Texture rImg(higashi);
+  const Texture lImg(nemu);
+  const Font Agents(50);
+  const Font str40(40);
+  String str1, str2;
 
+  str1 = Format(b1);
+  str2 = Format(b2);
+
+  selectImg(b1, b2);
   rect.draw(Palette::Skyblue);
 
-  rImg.draw(x + 200, y + 70);
-  lImg.draw(x + 50, y + 70);
+  rImg.draw(x + 220, y + 80);
+  lImg.draw(x + 40, y + 80);
 
-  rUpp(U'R').draw(x + 180, y, Palette::Black);
-  lUpp(U'L').draw(x, y, Palette::Black);
+  Agents(U"B2").draw(x + 190, y, Palette::Black);
+  Agents(U"B1").draw(x, y, Palette::Black);
+
+  str40(str1).draw(x + 140, y + 30, Palette::Crimson);
+  str40(str2).draw(x + 320, y + 30, Palette::Crimson);
+  
+  if (b1 >= 6){
+    str40(U"裏").draw(x + 80, y + 30, Palette::Black);
+  }
+  if (b2 >= 6){
+    str40(U"裏").draw(x + 260, y + 30, Palette::Black);
+  }
 }  
 }
