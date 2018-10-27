bc8ec52967d0cde054ff59127bccd1706eb688ac
higashi000 <higashisystem@gmail.com>
2018-10-28 00:43:29 +0900

    [add] メンバの追加

--- kyon/src/Game.hpp
+++ kyon/src/Game.hpp
@@ -29,6 +29,7 @@ public:
   Button enterButton;
   Button toggleColorButton;
   Button toggleLRButton;
+  Button allMove;
   Font turnNum;
   Font pointSumLabel;
   Font turnLabel;
@@ -48,6 +49,7 @@ public:
   void postTurnData();
   bool startGame(int32 x, int32 y);
   void finishTurn(int32 x, int32 y);
+  void myTeamAllMove(int32 x, int32 y);
   void undo(int32 x, int32 y);
   void toggleColor(int32 x, int32 y);
   void toggleLR(int32 x, int32 y);
